2fc223a5ba41da6d9adf0085487f3420144d0606
RAID_ATHALLAH <raidathallah036@gmail.com>
2025-04-20 23:17:19 +0700

    buat file env1, dan Tambahan Route Login, Register, dan Ubah kata sandi

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+# Route Login #
+Route::get('/login', [AuthController::class, 'showLoginForm'])
+->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+# Route Register #
+Route::get('/register', [AuthController::class, 'showRegisterForm'])
+->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+# Route Ubah Kata Sandi #
+Route::get('/reset-password', [AuthController::class, 'showResetPasswordForm'])
+->name('password.reset');
+Route::post('/reset-password', [AuthController::class, 'updatePassword'])->name('password.update');
