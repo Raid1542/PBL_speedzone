e7bf9ad3645f2777f24fe750803dca85cec75ef1
RAID_ATHALLAH <raidathallah036@gmail.com>
2025-05-15 08:08:45 +0700

    Route Admin

--- routes/web.php
+++ routes/web.php
@@ -62,5 +62,11 @@ Route::prefix('admin')->group(function () {
     Route::view('/riwayat-transaksi', 'admin.riwayat-transaksi')->name('admin.riwayat-transaksi');
     Route::view('/rekap-penjualan', 'pages.admin.rekap-penjualan')->name('admin.rekap-penjualan');
     Route::view('/konfirmasi_pembayaran', 'pages.admin.konfirmasi_pembayaran')->name('admin.konfirmasi_pembayaran');
+
+    Route::get('/produk', [ProdukController::class, 'index'])->name('admin.produk.index');  // Menampilkan daftar produk
+    Route::get('/produk/create', [ProdukController::class, 'create'])->name('admin.produk.create');  // Menampilkan form tambah produk
+    Route::post('/produk', [ProdukController::class, 'store'])->name('admin.produk.store');  // Menyimpan produk baru
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::get('/konfirmasi_pembayaran', [KonfirmasiPembayaranController::class, 'index'])->name('admin.konfirmasi.index');
 });
 
